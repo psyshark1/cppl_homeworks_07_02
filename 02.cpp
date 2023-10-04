f25aa77fd36a30804eafff74902dce79f97b081b
Andrey <andrey.zalozhenkov@pikta.ru>
2023-10-04 19:15:07 +0400

    Добавьте файлы проекта.

---
+++ 02.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <set>
+#include <algorithm>
+
+std::ostream& operator << (std::ostream& l, std::set<int, std::greater<int>>& r)
+{
+	for (const auto& it : r)
+	{
+		l << it << std::endl;
+	}
+	return l;
+}
+
+int main()
+{
+	std::cout << "[IN]: " << std::endl;
+	std::set<int, std::greater<int>> s;
+	int v{ 0 };
+	for (int i = 0; i < 10; ++i)
+	{
+		std::cin >> v;
+		s.insert(v);
+	}
+
+	std::cout << "[OUT]: " << std::endl;
+	std::cout << s;
+
+	return 0;
+}
